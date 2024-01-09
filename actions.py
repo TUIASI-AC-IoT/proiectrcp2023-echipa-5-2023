0857c9caee376a424bbd1bcae630b1fa215985fa
NafornitaAna <ana.nafornita@student.tuiasi.ro>
2024-01-09 16:42:01 +0200

    citire continut

--- actions.py
+++ actions.py
@@ -13,8 +13,6 @@ def delete_file(folder_path, file_name):
         print(f"The specified file does not exist: {file_path}")
     return False
 
-import os
-
 def create_empty_file(folder_path, file_name):
     file_path = os.path.join(folder_path, file_name)
     try:
@@ -26,7 +24,6 @@ def create_empty_file(folder_path, file_name):
         print(f"Failed to create empty file: {file_path}. {e}")
         return False
 
-import os
 
 def update_file(folder_path, file_name, new_content):
     file_path = os.path.join(folder_path, file_name)
@@ -38,5 +35,21 @@ def update_file(folder_path, file_name, new_content):
     except OSError as e:
         print(f"Failed to update file: {file_path}. {e}")
         return False
+
+
+def read_file_content(folder_path, file_name):
+    file_path = os.path.join(folder_path, file_name)
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+            return content
+        except OSError as e:
+            print(f"Failed to read file: {file_path}. {e}")
+            return None
+    else:
+        print(f"The specified file does not exist: {file_path}")
+        return None
+
 
 
