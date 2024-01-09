5afe76bb42766d32d4a1b440a94c427a5258fcb9
NafornitaAna <ana.nafornita@student.tuiasi.ro>
2024-01-09 16:04:02 +0200

    stergere

---
+++ actions.py
@@ -0,0 +1,15 @@
+import os
+
+def delete_file(folder_path, file_name):
+    file_path = os.path.join(folder_path, file_name)
+    if os.path.exists(file_path):
+        try:
+            os.remove(file_path)
+            print(f"Deleted file: {file_path}")
+            return True
+        except OSError as e:
+            print(f"Failed to delete file: {file_path}. {e}")
+    else:
+        print(f"The specified file does not exist: {file_path}")
+    return False
+
